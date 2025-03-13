328c8524f53b1ead0e5a143f76a1a0a309323b01
skrpov <skarpov03@gmail.com>
2025-03-13 00:13:15 -0700

    [C] Settings menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,20 +1,61 @@
 using UnityEngine;
+using UnityEditor;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int score = 0;
     [SerializeField] private CointCounterUI coinCounter;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject settingsMenu;
+    
+    private bool isSettingsMenuActive;
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
+        DisableSettingsMenu();
+    }
+
+    private void ToggleSettingsMenu() 
+    {
+        if (IsSettingsMenuActive) DisableSettingsMenu();
+        else EnableSettingsMenu();
+    }
+
+    private void EnableSettingsMenu() 
+    {
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isSettingsMenuActive = true;
+    }
+
+    public void DisableSettingsMenu() 
+    {
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isSettingsMenuActive = false;
     }
 
     public void IncreaseScore()
     {
         score++;
         coinCounter.UpdateScore(score);
+    }
+
+    public void QuitGame() 
+    {
+#if UNITY_EDITOR 
+        EditorApplication.isPlaying = false;
+#else 
+        Application.Quit();
+#endif
     }
 }
